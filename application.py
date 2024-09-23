fcad0a1898d31bdb8e9193cb90b21807aff5bd53
Luis <gupiluan@hotmail.com>
2024-09-23 09:45:44 -0600

    Para produccion

--- application.py
+++ application.py
@@ -5,5 +5,5 @@ app : Flask = create_app()
 
 
 #DEV
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+#if __name__ == "__main__":
+ #   app.run(debug=True, host="0.0.0.0", port=8080)
